d8130fe281c2f5a1014b1977ab458016effbc120
hamid <hamid331994@gmail.com>
2019-12-12 01:20:38 +0330

    dev

--- src/components/Layer.js
+++ src/components/Layer.js
@@ -5,19 +5,39 @@ import Context from '../context/Context';
 import './layer.css';
 
 const Layer = () => {
-  const { state } = useContext(Context)
+  const { state, setState } = useContext(Context)
 
   const onbuttonClick = () => {
-    console.log('button clicked')
+    state === 'signin'
+      ? setState('signup')
+      : setState('signin')
   }
 
   return (
     <div id='layer' className={`${state}`}>
       <div className={`background ${state}`}>
+        <div className='shape triangle1'>
 
+        </div>
+        <div className='shape triangle2'>
+
+        </div>
+        <div className='shape circle'>
+
+        </div>
+        <div className='shape square'>
+
+        </div>
       </div>
       <button onClick={onbuttonClick} className={`${state}`}>
-        { state }
+        {
+          state === 'signin'
+            && 'sign up'
+        }
+        {
+          state === 'signup'
+            && 'sign in'
+        }
       </button>
     </div>
   )
