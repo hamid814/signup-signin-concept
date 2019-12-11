423bd715dd11ddd5dd7a3d87acb39ca92985aafe
hamid <hamid331994@gmail.com>
2019-12-11 22:35:06 +0330

    dev

--- src/components/Layer.js
+++ src/components/Layer.js
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, { useContext } from 'react'
+
+import Context from '../context/Context'; 
 
 import './layer.css';
 
 const Layer = () => {
+  const { state } = useContext(Context)
+
+  const onbuttonClick = () => {
+    console.log('button clicked')
+  }
+
   return (
     <div id='layer'>
       <div className="background">
-        
+
       </div>
+      <button onClick={onbuttonClick}>
+        { state }
+      </button>
     </div>
   )
 }
