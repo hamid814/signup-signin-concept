f48d76a78f0db2fdb215cfd42d19dc9b6fe1988f
hamid <hamid331994@gmail.com>
2019-12-11 10:02:20 +0330

    dev

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
+import Container from './components/Container';
+
 import State from './context/State';
 
 import './index.css';
@@ -9,7 +11,7 @@ import './index.css';
 function App() {
   return (
     <>
-      
+      <Container />
     </>
   );
 }
