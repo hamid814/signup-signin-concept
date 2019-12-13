71416328ad042ab8b422769c1e9359340465a976
hamid <hamid331994@gmail.com>
2019-12-14 00:27:11 +0330

    dev

--- src/components/layout/Container.js
+++ src/components/layout/Container.js
@@ -1,23 +1,19 @@
-import React, { useContext } from 'react'
+import React from 'react'
 
 import Layer from '../Layer';
-
-import Context from '../../context/Context';
 
 import './container.scss';
 
 const Container = () => {
-  const { state } = useContext(Context)
-
   return (
     <>
       <div id='app-container'>
         <Layer />
       </div>
-      <div className={`back-circle ${state === 'signin' ? 'red' : 'yellow'}`}>
+      <div className='back-circle'>
       
       </div>
-      <div className={`back-triangle ${state === 'signin' ? 'red' : 'yellow'}`}>
+      <div className='back-triangle'>
       
       </div>
     </>
