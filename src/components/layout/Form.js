a3d227f5c4408f13d50bf508bdea2a1fee4b8201
hamid <hamid331994@gmail.com>
2019-12-14 16:12:14 +0330

    dev

--- src/components/layout/Form.js
+++ src/components/layout/Form.js
@@ -19,6 +19,9 @@ const Form = () => {
       {
         SigninForm().text
       }
+      {
+        SigninForm().inputs
+      }
     </div>
   )
 }
--- src/components/signin/Form.js
+++ src/components/signin/Form.js
@@ -3,12 +3,35 @@ import React from 'react'
 import './form.scss';
 
 const SigninForm = () => {
-  const header = <h1>Create Account</h1>
-  const text = <div>or use your email for registration:</div>
+  const header = <h1 className='form-title'>Create Account</h1>
+
+  const text = <div className='form-desc'>or use your email for registration:</div>
+
+  const nameInput = <div className='form-group'>
+                      <span className='icon'>N</span>
+                      <input type='text' placeholder='name'/>
+                    </div>
+
+  const emailInput = <div className='form-group'>
+                      <span className='icon'>E</span>
+                      <input type='text' placeholder='Emial'/>
+                     </div>
+
+  const passwordInput = <div className='form-group'>
+                          <span className='icon'>P</span>
+                          <input type='password' placeholder='password'/>
+                        </div>
+
+  const inputs = <div>
+    { nameInput }
+    { emailInput }
+    { passwordInput }
+  </div>
 
   return {
     header,
     text,
+    inputs
   }
 }
 
