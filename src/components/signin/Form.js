9479d97fd43b9bd0cc915222c01cee0ed48fa830
hamid <hamid331994@gmail.com>
2019-12-14 14:13:20 +0330

    dev

--- src/components/signin/Form.js
+++ src/components/signin/Form.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import './form.scss';
+
 const SigninForm = () => {
   const header = <h1>Create Account</h1>
   const text = <div>or use your email for registration:</div>
