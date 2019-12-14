1c8323e6f20c4d721b037c0fb43676ea984ea2f1
hamid <hamid331994@gmail.com>
2019-12-14 20:46:05 +0330

    dev

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -3,9 +3,16 @@ import React from 'react'
 import './styles/header.scss';
 
 const Header = () => {
+  const onClick = () => {
+    window.open('https://selectoglobal.com/')
+  }
+
   return (
-    <div id='app-header'>
-      idea from .//.
+    <div id='app-header' onClick={onClick}>
+      Idea from <br /> SELECTO
+      <div className="circle">
+      
+      </div>
     </div>
   )
 }
